f971603ccd3381a7572c7fa5cc66c8f7686d53a5
shaden hazem <shaden.hazem@aiu.edu.eg>
2022-12-24 20:24:03 +0200

    commit

--- app.js
+++ app.js
@@ -4,4 +4,20 @@ const menuLinks = document.querySelector('.navbar__menu');
 menu.addEventListener('click', function() {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
+
+  // Automatic Slideshow - change image every 3 seconds
+var myIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slideshow");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  myIndex++;
+  if (myIndex > x.length) {myIndex = 1}
+  x[myIndex-1].style.display = "block";
+  setTimeout(carousel, 3000);
+}
 });
